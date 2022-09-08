a571e1983356a12023532f91cbc751cbb11f04f1
PABLO ONOFRE <Gmod9K>
2022-09-08 12:24:53 -0300

    exec1

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+class App extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            sistolica: null,
+            diastolica: null,
+            result: " ",
+        }
+    }
+
+    onChangeInput(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    verificarPressao() {
+        let resultPressao;
+        const { sistolica, diastolica } = this.state
+
+        if (sistolica < 130 && diastolica < 85) {
+            resultPressao = "Normal"
+        } else if (sistolica < 140 && diastolica < 90) {
+            resultPressao = "Normal - Limitrofe"
+        } else if (sistolica < 160 && diastolica < 100) {
+            resultPressao = "Hipertensão Leve"
+        } else if (sistolica < 180 && diastolica < 110) {
+            resultPressao = "Hipertensão Moderada"
+        } else {
+            resultPressao = "Hipertensão Grave"
+        }
+
+        this.setState({
+            result: resultPressao
+        })
+    }
+
+    showResult() {
+        if (this.state.result !== " ") {
+            return <p>Seu diagnóstico é: {this.state.result}</p>
+        }
+
+        return null
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Controle de Pressão Arterial</h1>
+
+                Pressão sistólica: <br />
+                <input type="number" name="sistolica"
+                    onChange={(event) => this.onChangeInput(event)} />
+
+                <br /><br />
+
+                Pressão diastólica: <br />
+                <input type="number" name="diastolica" 
+                    onChange={(event) => this.onChangeInput(event)} />
+
+                <br /><br />
+
+                <input type="button" value="Verificar"
+                    onClick={() => this.verificarPressao()} />
+
+                <br /><br />
+
+                {/* {
+                    this.state.result !== " "
+                        ? <p>Seu diagnostico e: {this.state.result}</p>
+                        : null    
+                } */}
+
+                {this.showResult()}
+
+            </div>
+        )
+    }
+}
+
+export default App;
